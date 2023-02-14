4fd1998d7901ce274a1ab094a896100e74a889c2
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-02-13 18:09:23 -0600

    Añadiendo State y retornando el Valor

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -15,7 +15,7 @@ text-transform: uppercase;
 font-size: 20px;
 border-radius: 5px;
 transition:  background-color .3s ease;
-
+margin-top: 30px ;
 &:hover {
     background-color: #7A7DFE;
     cursor: pointer;
@@ -27,13 +27,14 @@ transition:  background-color .3s ease;
 export const Formulario = () => {
 
 
-    const [SelectMonedas] = useSelectMonedas('Elige tu Moneda', monedas)
+    const [moneda, SelectMonedas] = useSelectMonedas('Elige tu Moneda', monedas)
 
-    SelectMonedas()
+    //SelectMonedas()
 
     return (
         <form>
             <SelectMonedas />
+          
             <InputSubmit
                 type="submit"
                 value="Cotizar"
